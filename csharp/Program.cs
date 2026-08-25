5e5f7617369164a5a39be63a14f134861fb3ed18
suresh.vakude <suresh.vakude@acty-sys.co.in>
2026-08-25 17:49:22 +0530

    roadmap added

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,1 +1,105 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/**
+
+1.Introduction
+2.CLR
+3.CTS
+4.CLS
+5.MSIL / CIL
+6.JIT
+7.Assemblies
+8.Namespaces
+9.Project structure
+10. Basic
+    Identifiers
+    Keywords
+    Variables
+    DataTypes
+    Constants and Literals
+    Type Casting
+    Comments
+    Operators
+
+11. Control Structures
+    Decision Making: If - Else, Switch - Case
+    Loops: For, While, Do-While
+    Foreach Loop
+    
+12. Functions
+    Functions
+    Function Parameters
+    Return Types
+    Recursion in
+
+13. Arrays & Strings
+    Introduction to Arrays
+    Jagged Arrays
+    Sorting an Array
+    Introduction to String
+    Verbatim String
+    String vs StringBuilder
+
+14. Object-Oriented Programming (OOP)
+    Class and Objects
+    Methods
+    Constructors & Destructors
+    Properties
+    Encapsulation
+    Inheritance
+    Polymorphism
+    Abstraction
+    Interface
+
+15. Collections Framework
+    List
+    ArrayList
+    SortedList
+    HashSet
+    SortedSet
+    Dictionary
+    SortedDictionary
+    Hashtable
+    Stack
+    Queue
+    LinkedList
+    BitArray Class
+
+16. LINQ (Language Integrated Query)
+    Introduction
+    Query Syntax
+    Method Syntax
+    LINQ Operators and Methods
+    
+17. Generics
+    Introduction
+    Generic Classes
+    Generic Methods
+    Generic Constraints
+    Collections with Generics
+
+18. Exception Handling
+    Exception Handling (Try-catch-finally)
+    Multiple Catch Clause
+    Custom Exceptions
+
+19. Multithreading
+    Introduction
+    Types of Threads
+    Creating Threads
+    Main Thread
+    Lifecycle and States of Thread
+    Thread Class
+    Thread Priority
+    Thread Synchronization
+    Thread Safety & Race Conditions
+    Joining Threads
+    Terminating a Thread
+
+20. Advanced C#
+    Delegates
+    Lambda Expressions
+    Extension Methods
+    Events
+    Pattern Matching
+    Attributes
+
+**/
